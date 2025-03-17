0113e94224ddcf3cc42476d100ae073cf238d4cd
jihwantime <jihwanlzh@gmail.com>
2025-03-17 06:53:45 +0000

    added verifyword ladder

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -34,11 +34,6 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
     vector<int> path;
 
-    // if (destination < 0 || destination >= static_cast<int>(distances.size()) || 
-    //     distances[destination] == INF) {
-    //     return path; 
-    // }
-    
     for (int v = destination; v != -1; v = previous[v]) {
         path.push_back(v);
     }
