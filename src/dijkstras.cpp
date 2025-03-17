7a434ad286f248058d3dace46e97d0eb875327ba
jihwantime <jihwanlzh@gmail.com>
2025-03-17 05:46:06 +0000

    fixing dijkstras logic

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -3,32 +3,38 @@
 #include <set>
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
-    vector<int> dist(G.numVertices, INF);
-    previous.resize(G.numVertices, -1);
+    int n = G.numVertices;
+    vector<int> dist(n, INF);
+    previous.resize(n, -1);
+    vector<bool> visited(n, false);
+    
     dist[source] = 0;
-
+    
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     pq.push({0, source});
-
+    
     while (!pq.empty()) {
         int u = pq.top().second;
-        int dist_u = pq.top().first;
         pq.pop();
-
-        if (dist_u > dist[u]) continue;
-
+        
+        if (visited[u]) {
+            continue;
+        }
+        
+        visited[u] = true;
+        
         for (const Edge& edge : G[u]) {
             int v = edge.dst;
             int weight = edge.weight;
-
-            if (dist[u] != INF && dist[u] + weight < dist[v]) {
+            
+            if (!visited[v] && dist[u] != INF && dist[u] + weight < dist[v]) {
                 dist[v] = dist[u] + weight;
                 previous[v] = u;
                 pq.push({dist[v], v});
             }
         }
     }
-
+    
     return dist;
 }
 
@@ -40,18 +46,11 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
         return path; 
     }
     
-    set<int> visited;
-    
     for (int v = destination; v != -1; v = previous[v]) {
-        if (visited.find(v) != visited.end()) {
-            return vector<int>();
+        if (path.size() > distances.size()) {
+            return vector<int>(); 
         }
-        visited.insert(v);
         path.push_back(v);
-        
-        if (previous[v] >= static_cast<int>(previous.size())) {
-            return vector<int>();
-        }
     }
     
     reverse(path.begin(), path.end());
